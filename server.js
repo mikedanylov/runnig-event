889e7952cabbaa7de1381fa22e250d2a61d42775
Mykhailo Danylov <mike.danylov@gmail.com>
2015-11-20 21:37:50 +0200

    First working proto

--- server.js
+++ server.js
@@ -27,5 +27,5 @@
 
     // application -------------------------------------------------------------
     app.get('*', function(req, res) {
-        res.sendfile('index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendfile('public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
