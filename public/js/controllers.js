587843aa7879736fc4c927ea4b5c0ff1e0eaf10a
Mykhailo Danylov <mike.danylov@gmail.com>
2015-11-21 21:52:10 +0200

    Added infinite scroll

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,5 +1,10 @@
 (function(){
-  var app = angular.module('runningEventsApp', []);
+  var app = angular.module('runningEventsApp', ['infinite-scroll']);
+
+  // app.config(['$httpProvider', function($httpProvider) {
+  //   $httpProvider.defaults.useXDomain = true;
+  //   delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  // }]);
 
   app.controller('EventsController', ['$scope', '$http', '$timeout', '$q', function ($scope, $http, $timeout, $q) {
 
@@ -8,29 +13,53 @@
     $scope.totalDist = 0;
     $scope.numEvents = 0;
     $scope.geocoder = new google.maps.Geocoder();
+    $scope.eventsDisplayed = []
+
+    // make http request to api
+    // $http.jsonp("https://api-test.mynextrun.com/site/v1/event-stats").then(function(response){
+    //   console.log('Success!');
+    //   console.log(response);
+    // }, function(response){
+    //   console.log('Failed!');
+    //   console.log(response);
+    // });
 
     // get json data about running events
+    var fetchCount = 0;
+
+    // works as expected
     getEventsData('js/api-test.json');
+    // getEventsData("http://jsonplaceholder.typicode.com/posts");
     
-    // wait until response with events list is received
-    asyncWaitEvents().then(function(response){
-      console.log('Success: ' + response);
-      console.log($scope.eventsList);
-      $scope.eventsList.forEach(function(each){
-        $timeout(getEventLocation(each), 200);
-      });
-    }, function(response){
-      console.log('Failed: ' + response);
-    });
+    // doesn't work presumably because server does not allow cross-origin resource sharing
+    // or returns not formated JSON
+    // getEventsData("https://api-test.mynextrun.com/site/v1/event-stats");
+
+
+    $scope.loadMore = function(){
+      console.log("loading more");
+      var lastIndex = $scope.eventsDisplayed.length - 1;
+      if (lastIndex <= $scope.eventsList.length - 1){
+        for(var i = 0; i < 1; i++) {
+          $scope.eventsDisplayed.push($scope.eventsList[lastIndex + i]);
+        }  
+      }
+    }
 
     function getEventsData(url){
       $http.get(url).then(function(response){
+        // console.log(response);
         $scope.eventsList = response.data.events;
         $scope.totalDist = response.data.distanceCount;
         $scope.numEvents = response.data.eventCount;
+        $scope.eventsDisplayed = $scope.eventsList.slice(0, 4);
       }, function(response){
-        console.log('Failed to fetch events: ' + response);
-        $timeout(getEventsData(url), 200);
+        console.log('Failed to fetch events');
+        console.log(response);
+        if (fetchCount < 10){
+          $timeout(getEventsData(url), 200);
+          fetchCount++;
+        }
       });
     }
 
@@ -54,13 +83,11 @@
 
     function asyncWaitEvents() {
       return $q(function(resolve, reject) {
-        $timeout(function() {
-          if (eventsLoaded()){
-            resolve(true);
-          } else {
-            reject(false);
-          }
-        }, 100);
+        if (eventsLoaded()){
+          resolve(true);
+        } else {
+          reject(false);
+        }
       });
     }
 
