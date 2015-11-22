cb17ce823bbe11963691034d22b84a473c63446f
Mykhailo Danylov <mike.danylov@gmail.com>
2015-11-22 17:20:08 +0200

    Fix for mongolab

--- models.js
+++ models.js
@@ -1,9 +1,22 @@
 var mongoose = require('mongoose');
 var _ = require('underscore');
+var uristring =
+    // process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    // process.env.PROD_MONGODB ||
+    'mongodb://localhost:27017/running-events';
 
 module.exports = function(wagner) {
-  // mongoose.connect('mongodb://localhost:27017/running-events');
-  mongoose.connect(process.env.MONGOLAB_URI);
+
+  // Makes connection asynchronously.  Mongoose will queue up database
+  // operations and release them when the connection is complete.
+  mongoose.connect(uristring, function (err, res) {
+    if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+    console.log ('Succeeded connected to: ' + uristring);
+    }
+  });
 
   var Event =
     mongoose.model('Event', require('./schema/Event'), 'Events');
