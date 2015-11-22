7fe340989e634365aa14913c7d700b2d5b577cca
Mykhailo Danylov <mike.danylov@gmail.com>
2015-11-22 15:52:59 +0200

    Fix for mongolab

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ var app = express();
 
 app.use('/api/', require('./api')(wagner));
 app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res) {
-  res.sendfile('public/index.html');
-});
+// app.get('/', function(req, res) {
+//   res.sendfile('public/index.html');
+// });
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
--- models.js
+++ models.js
@@ -2,7 +2,11 @@ var mongoose = require('mongoose');
 var _ = require('underscore');
 
 module.exports = function(wagner) {
-  mongoose.connect('mongodb://localhost:27017/running-events');
+  // mongoose.connect('mongodb://localhost:27017/running-events');
+  mongoose.connect(
+    // 'mongodb://mikedanylov:ps7vj590mongolab@ds053838.mongolab.com:53838/heroku_2f06jnj3'
+    'mongodb://heroku_2f06jnj3:9e0hejakhjjqqll19savjbfsh8@ds053838.mongolab.com:53838/heroku_2f06jnj3' 
+  );
 
   var Event =
     mongoose.model('Event', require('./schema/Event'), 'Events');
