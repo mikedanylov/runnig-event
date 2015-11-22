ec80cec45969a8cb12f21b18cf5d5bffb7a11c36
Mykhailo Danylov <mike.danylov@gmail.com>
2015-11-22 15:01:36 +0200

    Nodejs REST

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var wagner = require('wagner-core');
 var mongoose = require('mongoose');
 var port = process.env.PORT || 8080;
-var EventSchema = require('./schema/Event.js');
+// var EventSchema = require('./schema/Event.js');
 
 require('./models')(wagner);
 
@@ -12,6 +12,9 @@ var app = express();
 
 app.use('/api/', require('./api')(wagner));
 app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
+  res.sendfile('public/index.html');
+});
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
